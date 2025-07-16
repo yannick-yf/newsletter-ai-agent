501023135f30a5a83f75e8f0aa5154eb1b6a1079
Yannick Flores <yannick.flores1992@gmail.com>
2025-07-16 11:52:02 +0200

    Update cron task hour. Add new senders

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ load_dotenv()
 success = send_gmail(
     sender_email=os.getenv('GMAIL_EMAIL'),
     sender_password=os.getenv('GMAIL_APP_PASSWORD'),
-    recipient_email=["ipfy.solutions@gmail.com", 'yannick.flores1992@gmail.com'],
+    recipient_email=["ipfy.solutions@gmail.com", 'yannick.flores1992@gmail.com', 'super_aleoup@hotmail.com'],
     subject="Email Test",
     body="This is the plain text version.",
     html_body=output_formating()
